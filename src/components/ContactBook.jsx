abefd28ff8dcbb64f0784ba82bdee5e3109b9a43
Michael Steven Pineda Torres <stevenpnd@gmail.com>
2022-02-07 21:38:04 -0500

    Create contacts reducer

--- src/components/ContactBook.jsx
+++ src/components/ContactBook.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { contactsReducer as reducer } from '../reducers/contactsReducer';
 import ContactForm from './ContactForm';
 import Contacts from './Contacts';
 
 const ContactBook = () => {
 
-    const contacts = [
-        {id: "1", name: "Michael", number: "3148000279"},
-        {id: "2", name: "Jois", number: "3108000279"}
+    const initialContacts = [
+        { id: "1", name: "Michael", number: "3148000279" },
+        { id: "2", name: "Jois", number: "3108000279" }
     ];
+
+    const [contacts, dispatch] = useReducer(reducer, initialContacts);
 
     return (
         <div className="container">
---
+++ src/reducers/contactsReducer.js
@@ -0,0 +1,11 @@
+export const contactsReducer = (state, action) => {
+    switch (action.type) {
+        case "add":
+            return [...state, action.payload];
+        case "delete":
+            const { id } = action.payload;
+            return state.filter(contact => contact.id !== id);
+        default:
+            return state;
+    }
+};
